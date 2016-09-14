6c479658801ef7b6a3186a82d3d716cdd54cb1ad
Fercho <gomez.alejandre@gmail.com>
2016-09-14 16:08:44 -0500

    Cambios en interfaz y agrero librerias de pdf

--- Secuencia/src/Interfaz/MenuRubrica.java
+++ Secuencia/src/Interfaz/MenuRubrica.java
@@ -5,21 +5,18 @@
  */
 package Interfaz;
 
-import Fachada.Asignatura;
+import Fachada.*;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultComboBoxModel;
-import javax.swing.JComboBox;
-import javax.swing.JPanel;
 
 /**
  *
- * @author DARKENSES
+ * @author Luis Fernando
  */
-public class MenuRubrica extends javax.swing.JFrame {
+public class MenuRubrica extends javax.swing.JFrame{
 
     Asignatura asignatura;
     List<String> asignaturas;
@@ -29,8 +26,8 @@ public class MenuRubrica extends javax.swing.JFrame {
      *
      * @throws java.io.IOException
      */
-    public MenuRubrica() throws IOException {
-        asignatura = new Asignatura();
+    public MenuRubrica() throws IOException{
+        Asignatura asignatura = new Asignatura();
         asignaturas = asignatura.listarAsignaturas();
         initComponents();
     }
@@ -44,36 +41,36 @@ public class MenuRubrica extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
-        jComboBox2 = new javax.swing.JComboBox<>();
+        semestreLabel = new javax.swing.JLabel();
+        bloqueLabel = new javax.swing.JLabel();
+        asignaturaLabel = new javax.swing.JLabel();
+        docenteLabel = new javax.swing.JLabel();
+        semestreComboBox = new javax.swing.JComboBox<>();
+        bloqueComboBox = new javax.swing.JComboBox<>();
         asignaturaComboBox = new javax.swing.JComboBox<>();
         docenteTextField = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jLabel5 = new javax.swing.JLabel();
-        jSeparator1 = new javax.swing.JSeparator();
-        jLabel6 = new javax.swing.JLabel();
+        iniciarRubricaButton = new javax.swing.JButton();
+        subtituloLabel = new javax.swing.JLabel();
+        botoneraSeparator = new javax.swing.JSeparator();
+        instruccionesLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel1.setText("Semestre");
+        semestreLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        semestreLabel.setText("Semestre");
 
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel2.setText("Bloque");
+        bloqueLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        bloqueLabel.setText("Bloque");
 
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel3.setText("Asignatura");
+        asignaturaLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        asignaturaLabel.setText("Asignatura");
 
-        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel4.setText("Docente");
+        docenteLabel.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        docenteLabel.setText("Docente");
 
-        jComboBox1.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        semestreComboBox.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
 
-        jComboBox2.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        bloqueComboBox.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
 
         asignaturaComboBox.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
         asignaturaComboBox.setModel(new DefaultComboBoxModel(asignaturas.toArray()));
@@ -81,25 +78,20 @@ public class MenuRubrica extends javax.swing.JFrame {
         docenteTextField.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
         docenteTextField.setToolTipText("");
 
-        jButton1.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        jButton1.setText("Iniciar Rubrica");
-        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+        iniciarRubricaButton.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        iniciarRubricaButton.setText("Iniciar Rubrica");
+        iniciarRubricaButton.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton1MouseClicked(evt);
-            }
-        });
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                iniciarRubricaButtonMouseClicked(evt);
             }
         });
 
-        jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
-        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel5.setText("Sistema de Secuencias");
+        subtituloLabel.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
+        subtituloLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        subtituloLabel.setText("Sistema de Secuencias");
 
-        jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        jLabel6.setText("Primero Elija la Asignatura Para Listar Semestres");
+        instruccionesLabel.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        instruccionesLabel.setText("Primero Elija la Asignatura Para Listar Semestres");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -107,63 +99,63 @@ public class MenuRubrica extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                .addComponent(iniciarRubricaButton)
                 .addContainerGap())
-            .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.TRAILING)
+            .addComponent(botoneraSeparator, javax.swing.GroupLayout.Alignment.TRAILING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(44, 44, 44)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 485, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(subtituloLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 485, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel3)
-                            .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(asignaturaLabel)
+                            .addComponent(bloqueComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addComponent(jLabel1)
+                                        .addComponent(semestreLabel)
                                         .addComponent(asignaturaComboBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addComponent(jLabel2))
+                                        .addComponent(semestreComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addComponent(bloqueLabel))
                                 .addGap(38, 38, 38)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel6)
+                                    .addComponent(instruccionesLabel)
                                     .addComponent(docenteTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 293, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jLabel4))))
+                                    .addComponent(docenteLabel))))
                         .addContainerGap(24, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel5)
+                .addComponent(subtituloLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel3)
+                .addComponent(asignaturaLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(asignaturaComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel4))
+                            .addComponent(semestreLabel)
+                            .addComponent(docenteLabel))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(semestreComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jLabel2)
+                                .addComponent(bloqueLabel)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(bloqueComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addComponent(docenteTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 23, Short.MAX_VALUE)
-                        .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(botoneraSeparator, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(7, 7, 7)
-                        .addComponent(jButton1))
+                        .addComponent(iniciarRubricaButton))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel6)
+                        .addComponent(instruccionesLabel)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -171,25 +163,20 @@ public class MenuRubrica extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+    private void iniciarRubricaButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_iniciarRubricaButtonMouseClicked
         Rubrica rubrica = new Rubrica();
-        Asignatura asignatura = new Asignatura();
         Rubrica.asignaturaTextField.setText((String) asignaturaComboBox.getSelectedItem());
         asignatura.setAsignatura((String) asignaturaComboBox.getSelectedItem());
         Rubrica.docenteTextField.setText(docenteTextField.getText());
-        
+
         rubrica.setVisible(true);
         this.setVisible(false);
-    }//GEN-LAST:event_jButton1MouseClicked
+    }//GEN-LAST:event_iniciarRubricaButtonMouseClicked
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String args[]){
         /*
          * Set the Nimbus look and feel
          */
@@ -199,20 +186,20 @@ public class MenuRubrica extends javax.swing.JFrame {
          * default look and feel. For details see
          * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+        try{
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()){
+                if ("Nimbus".equals(info.getName())){
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
+        }catch (ClassNotFoundException ex){
             java.util.logging.Logger.getLogger(MenuRubrica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
+        }catch (InstantiationException ex){
             java.util.logging.Logger.getLogger(MenuRubrica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
+        }catch (IllegalAccessException ex){
             java.util.logging.Logger.getLogger(MenuRubrica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        }catch (javax.swing.UnsupportedLookAndFeelException ex){
             java.util.logging.Logger.getLogger(MenuRubrica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
@@ -221,11 +208,11 @@ public class MenuRubrica extends javax.swing.JFrame {
         /*
          * Create and display the form
          */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
+        java.awt.EventQueue.invokeLater(new Runnable(){
+            public void run(){
+                try{
                     new MenuRubrica().setVisible(true);
-                } catch (IOException ex) {
+                }catch (IOException ex){
                     Logger.getLogger(MenuRubrica.class.getName()).log(Level.SEVERE, null, ex);
                 }
 
@@ -236,16 +223,16 @@ public class MenuRubrica extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public static javax.swing.JComboBox<String> asignaturaComboBox;
+    private javax.swing.JLabel asignaturaLabel;
+    private javax.swing.JComboBox<String> bloqueComboBox;
+    private javax.swing.JLabel bloqueLabel;
+    private javax.swing.JSeparator botoneraSeparator;
+    private javax.swing.JLabel docenteLabel;
     private javax.swing.JTextField docenteTextField;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JComboBox<String> jComboBox2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JButton iniciarRubricaButton;
+    private javax.swing.JLabel instruccionesLabel;
+    private javax.swing.JComboBox<String> semestreComboBox;
+    private javax.swing.JLabel semestreLabel;
+    private javax.swing.JLabel subtituloLabel;
     // End of variables declaration//GEN-END:variables
 }
--- Secuencia/src/Interfaz/Rubrica.java
+++ Secuencia/src/Interfaz/Rubrica.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package Interfaz;
-import Interfaz.MenuRubrica;
+import Fachada.*;
 /**
  *
  * @author DARKENSES
